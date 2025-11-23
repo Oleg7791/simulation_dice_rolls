604a68c125356ea6a6d64574dd51f9f1d0e21c99
oleg <o1egov@mail.ru>
2025-11-23 18:59:19 +0700

    создали класс кубик

--- die.py
+++ die.py
@@ -1,13 +1,13 @@
-import random
+from random import randint
 
 
 class Die:
-    """ класс представляющий один кубик"""
+    """класс представляющий кубик"""
 
     def __init__(self, num_sides=6):
-        """по умолчанию используется шестигранный кубик"""
+        """по умолчанию используем один кубик"""
         self.num_sides = num_sides
 
     def roll(self):
-        """возвращает случайное число от1 до num_sides"""
-        return random.randint(1, self.num_sides)
+        """возвращает случайное число от 1 до num_sides"""
+        return randint(1, self.num_sides)
