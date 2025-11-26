18224606965b259346e417b2ef52b59584d5d57b
oleg <o1egov@mail.ru>
2025-11-26 18:57:36 +0700

    постройка гистограммы

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # создание кубика
@@ -13,4 +16,14 @@ frequencies = []
 for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# визуализация результатов
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': "Frequency of Result"}
+my_laylout = Layout(title='Results of rolling one D6 1000 times',
+                    xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_laylout}, filename='d6.html')
 print(frequencies)
