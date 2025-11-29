ed4a06b4ea2bcb844906e1e24527f600256d53f6
oleg <o1egov@mail.ru>
2025-11-29 12:05:29 +0700

    бросок двух кубиков.

--- die_visual.py
+++ die_visual.py
@@ -4,26 +4,29 @@ from plotly import offline
 from die import Die
 
 # создание кубика
-die = Die()
+die1 = Die()
+die2 = Die()
 
 # моделирование серии бросков
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die1.roll() + die2.roll()
     results.append(result)
 # анализ результатов
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_result = die1.num_sides + die2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # визуализация результатов
-x_values = list(range(1, die.num_sides + 1))
+x_values = list(range(2, max_result + 1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': "Frequency of Result"}
-my_laylout = Layout(title='Results of rolling one D6 1000 times',
+my_layout = Layout(title='Results of rolling two D6 1000 times',
                     xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_laylout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout},
+             filename='d6_d6.html')
 print(frequencies)
